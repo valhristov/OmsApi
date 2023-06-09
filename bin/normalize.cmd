c28adbc561c5d723c96d506fdf5005f70dbe0d12
Valeri Hristov <valeri.hristov@inexto.com>
2023-06-09 10:15:01 +0200

    Update ApiNormalizer.exe

--- bin/normalize.cmd
+++ bin/normalize.cmd
@@ -3,8 +3,10 @@ pushd %~dp0
 cd.. 
 
 set %date% = date /t
-
-bin\ApiNormalizer.exe .
+bin\ApiNormalizer.exe ru . "https://suz.sandbox.crptech.ru"
+bin\ApiNormalizer.exe kg . "https://oms.megacom.kg"
+bin\ApiNormalizer.exe kz . "https://suzcloud.stage.ismet.kz"
+bin\ApiNormalizer.exe uz . "https://omscloud.asllikbelgisi.uz"
 
 git add .
 git commit -m %date%
